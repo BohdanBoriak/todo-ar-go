8982fe717ddb1811bb4405bd680c58babf163234
BohdanBoriak <lightraven72@gmail.com>
2024-03-18 10:32:55 +0200

    Added: container

--- main.go
+++ main.go
@@ -3,35 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
-	"todo-list/config"
+	"todo-list/config/container"
 	"todo-list/domain"
-	database "todo-list/repository"
-
-	"github.com/upper/db/v4/adapter/postgresql"
 )
 
 func main() {
-	config := config.GetConfig()
+	cont := container.New()
 
-	var settings = postgresql.ConnectionURL{
-		Database: config.DbName,
-		Host:     config.DbHost,
-		User:     config.DbUser,
-		Password: config.DbPassword,
-	}
-
-	sess, err := postgresql.Open(settings)
-	if err != nil {
-		log.Fatalf("Couldn't establish db connection")
-	}
-
-	ur := database.NewUserRepository(sess)
 	u := domain.User{
 		Name:     "Bohdan",
 		Password: "1234",
 	}
 
-	user, err := ur.Save(u)
+	user, err := cont.UserRepo.Save(u)
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
